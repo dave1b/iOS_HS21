c5e97a836b3c406bfacc01a8154e3dee8f4870fb
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-19 20:45:10 +0200

    implemented the UserDefault the right way

--- SwiftUIWidgets/SwiftUIWidgets/MyGlobalValues.swift
+++ SwiftUIWidgets/SwiftUIWidgets/MyGlobalValues.swift
@@ -6,22 +6,34 @@
 //
 
 import Foundation
+import WidgetKit
  
 class MyGlobalValues: ObservableObject  {
-    
+    private let userDefault = UserDefaults(suiteName: "group.ch.hslu.ios.SwiftUIWidgets")
     
     
     @Published var backGroundColor = "Color.gray"
-    @Published var counter: Int = 0
-    
-    @Published var iconOptions = ["☮️","🎶","🐰","💎"]
-    @Published var selectedIcon : String = "☮️"
-    
-    init() {
-        self.backGroundColor = UserDefaults.standard.object(forKey: "backGroundColor") as? String ?? ""
-        //self.backGroundColor = UserDefaults.standard.object(forKey: "iconOptions") as? String ?? ""
-        self.selectedIcon = UserDefaults.standard.object(forKey: "selectedIcon") as? String ?? ""
-        self.counter = UserDefaults.standard.object(forKey: "counter") as? Int ?? 0
+    @Published var counter: Int = 0 {
+        didSet{
+            userDefault?.set(counter, forKey: "counter")
+            //self.counter = UserDefaults.standard.object(forKey: "counter") as? Int ?? 0
+            WidgetCenter.shared.reloadAllTimelines()
+        //WidgetCenter.shared.reloadTimelines(ofKind: "MyWidget")
+    }
     }
     
+    
+    @Published var iconOptions = ["☮️","🎶","🐰","💎"]
+    @Published var selectedIcon : String = "☮️"{
+        didSet{
+            userDefault?.set(selectedIcon, forKey: "selectedIcon")
+            //WidgetCenter.shared.reloadAllTimelines()
+           // self.selectedIcon = UserDefaults.standard.object(forKey: "selectedIcon") as? String ?? ""
+        WidgetCenter.shared.reloadTimelines(ofKind: "MyWidget")
+    }
+    }
+    init() {
+        self.backGroundColor = UserDefaults.standard.object(forKey: "backGroundColor") as? String ?? ""
+        //self.iconOptions = UserDefaults.standard.object(forKey: "iconOptions") as? [String] ?? [""]
+    }
 }
